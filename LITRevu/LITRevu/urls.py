ae1d73f2ccc18a056d16c9238180d225966509ef
NZT48DEV <nzt48.dev@gmail.com>
2025-09-10 16:15:37 +0200

    Création de la vue de connexion, d'inscription, de déconnexion (base)

--- LITRevu/LITRevu/urls.py
+++ LITRevu/LITRevu/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView
+import authentication.views
+import app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LoginView.as_view(template_name='authentication/login.html',
+                               redirect_authenticated_user=True),
+                                name='login'),
+    path('logout/', authentication.views.logout_user, name='logout'),
+    path('signup/', authentication.views.signup_page, name='signup'),
+    path('home/', app.views.home, name='home'),
 ]
