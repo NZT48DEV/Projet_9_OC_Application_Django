22811743508d69811986feb8eb78e48685bc9a1b
NZT48DEV <nzt48.dev@gmail.com>
2025-09-12 12:12:11 +0200

    Mise en page de la création de ticket (bootstrap)

--- LITRevu/tickets/forms.py
+++ LITRevu/tickets/forms.py
@@ -1,12 +1,27 @@
 from django import forms
-from . import models
-
-class PhotoForm(forms.ModelForm):
-    class Meta:
-        model = models.Photo
-        fields = ['image']
+from .models import Ticket, Photo
 
 class TicketForm(forms.ModelForm):
     class Meta:
-        model = models.Ticket
+        model = Ticket
         fields = ['title', 'description', 'type']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'placeholder': 'Entrez le titre du ticket',
+                'class': 'form-control'
+            }),
+            'description': forms.Textarea(attrs={
+                'placeholder': 'Décrivez votre ressource...',
+                'class': 'form-control',
+                'rows': 5
+            }),
+            'type': forms.Select(attrs={'class': 'form-select'}),
+        }
+
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = ['image']
+        widgets = {
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'})
+        }
